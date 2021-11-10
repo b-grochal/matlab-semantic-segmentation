ee207d75b6e5de364600c91f10fe1b7e5627f877
Bartosz <radu480@o2.pl>
2021-11-10 17:50:05 +0100

    Implemented creation of all needed newtorks.

--- createNetworks.m
+++ createNetworks.m
@@ -1,8 +1,22 @@
-% Specify the network image size. This is typically the same as the traing image sizes.
-imageSize = [2178 4096 3];
+imageSize = [2176 4096 3];
+imageSizeFcn = [2176 4096];
 
-% Specify the number of classes.
 numberOfClasses = numel(classes);
 
-% Create DeepLab v3+.
-deeplab = deeplabv3plusLayers(imageSize, numberOfClasses, "resnet18");
+deeplab1 = deeplabv3plusLayers(imageSize, numberOfClasses, "resnet18", 'DownsamplingFactor',8);
+deeplab2 = deeplabv3plusLayers(imageSize, numberOfClasses, "resnet18", 'DownsamplingFactor',16);
+deeplab3 = deeplabv3plusLayers(imageSize, numberOfClasses, "resnet50", 'DownsamplingFactor',8);
+deeplab4 = deeplabv3plusLayers(imageSize, numberOfClasses, "resnet50", 'DownsamplingFactor',16);
+deeplab5 = deeplabv3plusLayers(imageSize, numberOfClasses, "xception", 'DownsamplingFactor',8);
+deeplab6 = deeplabv3plusLayers(imageSize, numberOfClasses, "xception", 'DownsamplingFactor',16);
+deeplab7 = deeplabv3plusLayers(imageSize, numberOfClasses, "inceptionresnetv2",  'DownsamplingFactor',8);
+deeplab8 = deeplabv3plusLayers(imageSize, numberOfClasses, "inceptionresnetv2", 'DownsamplingFactor',16);
+
+fcn1=fcnLayers(imageSizeFcn, numberOfClasses, 'Type','8s');
+fcn2=fcnLayers(imageSizeFcn, numberOfClasses, 'Type','16s');
+fcn3=fcnLayers(imageSizeFcn, numberOfClasses, 'Type','32s');
+
+unet=unetLayers(imageSize, numberOfClasses);
+
+segnet1=segnetLayers(imageSize, numberOfClasses, 'vgg16');
+segnet4=segnetLayers(imageSize, numberOfClasses, 'vgg19'); 
